abf6b0ea38c812b0b1263d989df737061a209e42
My Home <sreeshnaks2000@gmail.com>
2022-11-09 11:54:35 +0530

    Initial commit

---
+++ lib/table.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home :MyTable()));
+}
+
+class MyTable extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: DataTable(
+          columns: const <DataColumn>[
+            DataColumn(
+              label: Text(
+                'Name',
+                style: TextStyle(fontStyle: FontStyle.italic),
+              ),
+            ),
+            DataColumn(
+              label: Text(
+                'Age',
+                style: TextStyle(fontStyle: FontStyle.italic),
+              ),
+            ),
+            DataColumn(
+              label: Text(
+                'Role',
+                style: TextStyle(fontStyle: FontStyle.italic),
+              ),
+            ),
+          ],
+          rows: const <DataRow>[
+            DataRow(
+              cells: <DataCell>[
+                DataCell(Text('Sarah')),
+                DataCell(Text('19')),
+                DataCell(Text('')),
+              ],
+            ),
+            DataRow(
+              cells: <DataCell>[
+                DataCell(Text('Janine')),
+                DataCell(Text('43')),
+                DataCell(Text('Professor')),
+              ],
+            ),
+            DataRow(
+              cells: <DataCell>[
+                DataCell(Text('William')),
+                DataCell(Text('27')),
+                DataCell(Text('Associate Professor')),
+              ],
+            ),
+          ],
+        ));
+  }
+}
